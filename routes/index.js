30e9c0682eaa8a7e617adb127e8461be962c921f
semillkasz <Kaszandra@MacBook-Pro-2.local>
2016-05-10 22:02:14 +1200

    Accounts page
    - sample database added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET account page. */
+router.get('/account', function(req, res, next) {
+  res.render('account', { title: 'Buy and Sell', fname: 'Jenn', lname: 'Niven', address: 'Wellington, New Zealand', rating: '4.7/5.0'});
+});
+
 module.exports = router;
