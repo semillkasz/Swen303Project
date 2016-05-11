13a4a9e8603f9fe94ffd732216d1fcc645b9438e
Jovan Bogoievski <nzjovi@gmail.com>
2016-05-11 22:17:20 +1200

    Implemented basic search
    Can now search and display items in the database.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var search = require('../routes/search');
+var pg = require('pg').native;
+
+/*
+	Comment this out and replace with your own database if username/password
+	is different. Will be eventually replaced with the postgres database
+	on ECS servers.
+*/
+var database = "postgres://postgres:swen303@localhost:5432/303";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET search page. */
+router.get('/search', function(req, res) {
+	//Get the search string the user searched for.
+	//NOTE: String has to be exact as the label in the database (will fix..)
+	var searchString = req.query.searchString;
+	search.search(res, searchString, database, pg);
 });
 
 module.exports = router;
---
+++ routes/search.js
@@ -0,0 +1,29 @@
+module.exports = {
+	/*
+		Search function to return all listings of what was searched.
+	*/
+	search: function(res, searchString, database, pg){
+		pg.connect(database, function(err, client, done){
+			if(err){
+				console.error('Could not connect to the database');
+				console.error(err);
+				return;
+			}
+			console.log('Connected to database');
+			//Query the database for the item searched for
+			client.query("SELECT * FROM stock WHERE label = '"+searchString+"';", 
+			function(error, result){
+				done();
+				if(error){
+					console.error('Failed to execute query');
+					console.error(error);
+					return;
+				} else{
+					console.log(result.rows);
+					//Return all listings
+					res.render('search', { title: 'SWEN Shop | Search', listings: result.rows });
+				}
+			});
+		});
+	}
+};
