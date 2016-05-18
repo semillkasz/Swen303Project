14c91566647fd2675a98db433e593b33a7086b83
semillkasz <Kaszandra@MacBook-Pro-3.local>
2016-05-18 21:35:23 +1200

    Accounts page update
    Now gets the account page from account.js
    Few tweaks in the database
    Trying to display recently bought items (still in progress)

--- routes/accounts.js
+++ routes/accounts.js
@@ -2,25 +2,58 @@ module.exports = {
 	/*
 		Search function to return all listings of what was searched.
 	*/
-	accounts: function(req, res, next) {
-	pg.connect(connectionString, onConnect);
-	function onConnect(err, client, done) {
-		if (err) {
-			console.error(err);
-			process.exit(1);
-		}
-		else {
-			client.query("SELECT * FROM users;", function(error, result){
-			done();
-			if(error){
-				console.error('Failed to execute query');
-				console.error(error);
-				return;
+	accounts: function(req, res, database, pg) {
+		pg.connect(database, function(err, client, done){
+			if (err) {
+				console.error(err);
+				process.exit(1);
 			}
-			var queryResult = JSON.stringify(result.rows);
-			res.render('account', { title: 'Buy and Sell', fname: 'Jenn', lname: 'Niven', address: 'Wellington, New Zealand', rating: '4.7/5.0', info: queryResult});
-			console.log(result.rows);
+			else {
+				client.query("SELECT * FROM users;", function(error, result){
+					if(error){
+						console.error('Failed to execute query');
+						console.error(error);
+						return;
+					}
+					var qr = JSON.stringify(result.rows);
+					var queryResult = JSON.parse(qr);
+
+					var u_username = queryResult[10].username;
+		  			var u_realname = queryResult[10].realname;
+		  			var u_address = queryResult[10].address;
+		  			var u_rating = queryResult[10].rating;
+		  			var u_photo = queryResult[10].photo;
+
+		  			client.query("SELECT sid FROM transactions WHERE uid = 10;", function(error, result){
+		  				if(error){
+		  					console.error('Failed to execute query');
+		  					console.error(error);
+		  					return;
+		  				}
+		  				// for (var i = Things.length - 1; i >= 0; i--) {
+		  				// 	Things[i]
+		  				// 	// var photo = [];
+		  				// 	//for every item in results 
+		  				// 	// photo[i] = results[i].photourl
+		  				// }
+		  				client.query("SELECT photourl FROM stock WHERE sid = 10;", function(error, result){
+			  				done();
+			  				if(error){
+			  					console.error('Failed to execute query');
+			  					console.error(error);
+			  					return;
+			  				}
+			  				// photos: photo
+			  				// var sid = result.rows;
+			  				// var stock = [];
+
+			  				
+							res.render('account', { title: 'Buy and Sell', realname: u_realname, address: u_address, rating: u_rating, photoSRC: u_photo});
+							console.log(result.rows);	
+						});
+					});
+				});
+			}
 		});
-		};}
 	}
-};
+}
